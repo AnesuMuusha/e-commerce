32257db0c07fa5ef2f280aec45b8d1680781cd2c
AnesuMuusha <anesumuusha@gmail.com>
2023-06-08 18:08:55 +0200

    Created the WelcomeBanner

---
+++ frontend/src/components/WelcomeBanner.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const WelcomeBanner = () => {
+  return (
+    <div className="welcome-banner bg-banner">
+      <div className="container">
+        <h2 className="text-center">🔥👗FastFashionWholesale.com</h2>
+        <p className="text-center">
+        Say goodbye to runner fees and
+        hidden costs. Your fashion, your prices, no strings attached. Get your
+        fashion trends cheap! START YOUR BUSINESS TODAY!**
+        </p>
+        <p className="text-center">
+          Get your fashion trends cheap! <strong>START YOUR BUSINESS TODAY!</strong>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeBanner;
